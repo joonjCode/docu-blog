889339bb59affa561059f3df44fa1a6ee6f0ee48
Joon J <jtj308@naver.com>
2023-03-11 01:06:35 +0900

    feat: Changed url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://{site-name}.netlify.app',
+  url: 'https://juniz.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
