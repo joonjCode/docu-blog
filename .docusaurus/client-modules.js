3b3aefbd6e90542d584215900be2cfa3102f9f25
Joon J <jtj308@naver.com>
2023-03-11 00:45:39 +0900

    feat : inital commit for blog

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/joon/Projects/docu-blog/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/joon/Projects/docu-blog/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/joon/Projects/docu-blog/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/joon/Projects/docu-blog/src/css/custom.css'),
+];
